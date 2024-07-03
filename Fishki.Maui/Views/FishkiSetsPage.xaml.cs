c91deb343cd58958a863e23b1a149afbc9b6d3b9
Jakub Jachowicz <kubajachowicz39@gmail.com>
2024-07-03 20:37:31 +0200

    Fishki sets UI in progress

--- Fishki.Maui/Views/FishkiSetsPage.xaml.cs
+++ Fishki.Maui/Views/FishkiSetsPage.xaml.cs
@@ -1,30 +1,50 @@
+using Fishki.Maui.Models;
 using Fishki.Maui.Utils;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace Fishki.Maui.Views;
 
 public partial class FishkiSetsPage : ContentPage
 {
-	private readonly FishkiApiService _apiService;
-    
+    private readonly FishkiApiService _apiService;
+    private ObservableCollection<FishkiSet> _fishkiSets;
+
+    public ObservableCollection<FishkiSet> FishkiSets
+    {
+        get => _fishkiSets;
+        set
+        {
+            _fishkiSets = value;
+            OnPropertyChanged(nameof(FishkiSets));
+        }
+    }
+
     public FishkiSetsPage()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
 
+        _fishkiSets = new ObservableCollection<FishkiSet>();
         _apiService = new FishkiApiService();
-	}
 
+        BindingContext = this;
+    }
 
-    protected override async void OnAppearing()
+    protected override  void OnAppearing()
     {
         base.OnAppearing();
 
+        RefreshFishkiList();
+    }
+
+    private async void RefreshFishkiList()
+    {
         var apiResponse = await _apiService.GetAllSets();
         if (apiResponse != null)
         {
             foreach (var fishkiSet in apiResponse)
             {
-                Debug.WriteLine($"{fishkiSet.Name}, {fishkiSet.WordsCount}");
+                _fishkiSets.Add(fishkiSet);
             }
         }
     }
