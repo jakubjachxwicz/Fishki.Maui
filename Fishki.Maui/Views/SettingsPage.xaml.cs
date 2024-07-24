f96424228ca17b6380fe1143673ad8a7e3a160a4
Jakub Jachowicz <kubajachowicz39@gmail.com>
2024-07-25 00:38:24 +0200

    UI 'SettingsPage' na Androidzie

--- Fishki.Maui/Views/SettingsPage.xaml.cs
+++ Fishki.Maui/Views/SettingsPage.xaml.cs
@@ -4,8 +4,18 @@ namespace Fishki.Maui.Views;
 
 public partial class SettingsPage : ContentPage
 {
-	public ICommand ReturnButtonCommand => new Command(() => Shell.Current.GoToAsync(".."));
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string OldPassword { get; set; }
+    public string NewPassword { get; set; }
+    public string RepeatedNewPassword { get; set; }
+
+    public ICommand ReturnButtonCommand => new Command(() => Shell.Current.GoToAsync(".."));
 	public ICommand LogoutButtonCommand => new Command(LogoutHandler);
+	public ICommand UpdateUsernameCommand => new Command(UpdateUsernameHandler);
+	public ICommand UpdateEmailCommand => new Command(UpdateEmailHandler);
+	public ICommand UpdatePasswordCommand => new Command(UpdatePasswordHandler);
+    public ICommand DeleteAccountCommand => new Command(DeleteAccountHandler);
 
 
     public SettingsPage()
@@ -25,4 +35,24 @@ public partial class SettingsPage : ContentPage
         SecureStorage.Remove("auth_token");
 		await Shell.Current.GoToAsync(nameof(LoginPage));
 	}
+
+    private void UpdateUsernameHandler()
+    {
+        
+    }
+
+    private void UpdateEmailHandler()
+    {
+        
+    }
+
+    private void UpdatePasswordHandler()
+    {
+        
+    }
+
+    private void DeleteAccountHandler()
+    {
+
+    }
 }
